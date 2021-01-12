1103e8aa938c5f3170e1a6863c70f9d5380e0d7e
Everett X <everettxu@Everetts-Air.fios-router.home>
2021-01-12 01:20:26 -0500

    Cleanup

---
+++ MATLAB/Family-finding monoids/iso.m
@@ -0,0 +1,18 @@
+function [isomorphs] = iso(combo)
+    numElements = size(combo,1);
+    permutations = perms(1:numElements);
+    isomorphs = cell(1,0);
+    curIso = combo;
+    for p = 1:1:size(permutations,1)
+        for i = 1:1:size(combo,1)
+            for j = 1:1:size(combo,2)
+                if combo(i,j) ~= 0
+                    curIso(permutations(p,i),permutations(p,j)) = permutations(p,combo(i,j));
+                else
+                    curIso(permutations(p,i),permutations(p,j)) = 0;
+                end
+            end
+        end
+        [~,isomorphs] = inclusionDiff(isomorphs,curIso);
+    end
+end
--- MATLAB/Family-finding monoids/main.m
+++ MATLAB/Family-finding monoids/main.m
@@ -1,16 +1,20 @@
 clear;
 clc;
 
-numMonoids = 4;
+numMonoids = 2;
 
 results = cell(1,0);
 indices = zeros(1,(numMonoids - 1)^2);
 maxIndices = (numMonoids - 1) * ones(1,(numMonoids - 1)^2);
-while(~isequal(indices, maxIndices))
+while(true)
     if(testConsistency(reshape(indices,[numMonoids-1,numMonoids-1]),0) == 1)
         results{1,end+1} = reshape(indices,[numMonoids-1,numMonoids-1]);
     end
-    indices = oldCycleArray(indices,numMonoids);
+    if(~isequal(indices, maxIndices))
+        indices = oldCycleArray(indices,numMonoids);
+    else
+        break
+    end
 end
 clearvars indices maxIndices
 
@@ -25,22 +29,3 @@ for i = 1:1:size(nonIsoFams,2)
     disp(nonIsoFams{1,i});
 end
 clearvars i
-
-function [isomorphs] = iso(combo)
-    numElements = size(combo,1);
-    permutations = perms(1:numElements);
-    isomorphs = cell(1,0);
-    curIso = combo;
-    for p = 1:1:size(permutations,1)
-        for i = 1:1:size(combo,1)
-            for j = 1:1:size(combo,2)
-                if combo(i,j) ~= 0
-                    curIso(permutations(p,i),permutations(p,j)) = permutations(p,combo(i,j));
-                else
-                    curIso(permutations(p,i),permutations(p,j)) = 0;
-                end
-            end
-        end
-        [~,isomorphs] = inclusionDiff(isomorphs,curIso);
-    end
-end
